5ee32c9b6d6b01c8a00202f5021e117105cc5623
sebastianhoenig <90109108+sebastianhoenig@users.noreply.github.com>
2025-11-28 08:31:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ews-ml",
-    version="0.1.0",
-    description="Studying the mechanisms of chromatin changes in Ewing Sarcoma using deep learning",
-    author="Sebastian Hönig",
+    name="chiron3d",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.10',
